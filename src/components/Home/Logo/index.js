3c91e62bdb31ccc342558ec7f91b009d6279a55d
venkatesh3265 <venkats3265@gmail.com>
2022-07-03 16:22:45 +0530

    changes done by venkatesh

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,28 +1,26 @@
 import "./index.scss";
 //import LogoS from "../../../assets/images/logo-v.png";
 import { useEffect, useRef } from "react";
-import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
-import gsap from "gsap-trial";
 
 const Logo = () => {
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   console.log(outlineLogoRef);
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin);
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      });
-  }, []);
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1,
+  //     })
+  //     .from(outlineLogoRef.current, {
+  //       drawSVG: 0,
+  //       duration: 20,
+  //     });
+  // }, []);
   return (
     <div className="logo-container" ref={bgRef}>
       <svg
